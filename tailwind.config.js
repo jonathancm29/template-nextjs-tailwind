b4e01da5d8c00835d486c4ba79533fba82767581
Jonathan Cardenas <jonathan.cardenasm29@gmail.com>
2023-02-14 21:35:24 -0500

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        colorCustom: '#0F172A',
+        colorContrast: '#5C98B9'
+      }
+    }
   },
   plugins: []
 }
